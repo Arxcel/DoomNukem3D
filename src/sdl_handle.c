5e3fad61571bbb2d66bd27d345026855dcecd6d3
Vadim Kozlov <vadym.kozlov@envisiontec.de>
2019-02-25 18:51:40 +0200

    [renderer] started test

--- src/sdl_handle.c
+++ src/sdl_handle.c
@@ -14,12 +14,15 @@
 
 static void			key_down(int key, t_main *m)
 {
+	static int a = 1;
 	if (key == SDLK_ESCAPE)
 		m->sdl.running = 0;
 	else if (key == SDLK_w || key == SDLK_s || key == SDLK_a || key == SDLK_d
 				|| key == SDLK_q || key == SDLK_e || key == SDLK_SPACE)
 	{
 		m->sdl.changes = 1;
+		sdl_pixel_put(m->sdl.img, a, a, 255);
+		a++;
 	}
 	else if (key == SDLK_i || key == SDLK_k || key == SDLK_j || key == SDLK_l
 				|| key == SDLK_u || key == SDLK_o)
@@ -38,6 +41,12 @@ void				sdl_loop(t_main *m)
 		sdl_hook(m);
 		if (m->sdl.changes)
 		{
+			if(m->sdl.changes)
+			{
+				SDL_UpdateTexture(m->sdl.texture, NULL, m->sdl.img.pixels, m->sdl.img.w * sizeof(unsigned int));
+				SDL_RenderCopy(m->sdl.ren, m->sdl.texture, NULL, &m->ui.scene_place);
+				SDL_RenderPresent(m->sdl.ren);
+			}
 			m->sdl.changes = 0;
 		}
 	}
