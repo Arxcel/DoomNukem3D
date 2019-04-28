51e9f648858b9f968f8111df0fb1662cea3fd951
Olena BONDARENKO <olbondar@student.unit.ua>
2019-04-28 12:51:47 +0300

    additional weapon

--- src/hud_loader.c
+++ src/hud_loader.c
@@ -73,7 +73,7 @@ void				load_hud(t_main *m)
 		MSG(TTF_GetError());
 	load_hud_texture(&m->hud.surface_hud, "assets/hud/hud.png");
 	load_hud_texture(&m->hud.gun_surface, "assets/hud/pistol.png");
-	load_hud_texture(&m->hud.gun_surface2, "assets/hud/pistol_2.png");
+	// load_hud_texture(&m->hud.gun_surface2, "assets/hud/pistol_2.png");
 	load_hud_font(m, "assets/fonts/auto_digital.ttf", 42);
 }
 
